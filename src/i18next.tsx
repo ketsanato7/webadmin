27dbaf07b99f7d4a67bf0ce893bea56e41ae8085
ketsanato7 <ketsanato7@gmail.com>
2025-08-26 16:49:48 +0700

    Add files via upload

---
+++ src/i18next.tsx
@@ -0,0 +1,23 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translation from './language/en.json';
+
+import LanguageDetector from 'i18next-browser-languagedetector';
+i18next.use(initReactI18next).use(LanguageDetector).init({
+  lng: 'en', // if you're using a language detector, do not define the lng option
+  debug: true,   
+  interpolation: {
+    escapeValue: true, // not needed for react as it escapes by default
+  },
+  resources: {
+    en: {
+        translation,
+    },
+    la:{
+        translation,
+    }
+  },
+  // if you see an error like: "Argument of type 'DefaultTFuncReturn' is not assignable to parameter of type xyz"
+  // set returnNull to false (and also in the i18next.d.ts options)
+  // returnNull: false,
+});
